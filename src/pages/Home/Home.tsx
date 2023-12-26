27968446b59ad9243f2fefae231d11000780a90c
ParkYoDo <scarletdemon@naver.com>
2023-12-27 02:31:40 +0900

    Home Page

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,68 @@
+import { LayoutWrap } from '@styles/GlobalStyle';
+
+const Home = () => {
+  return (
+    <LayoutWrap
+      style={{
+        border: ' 5px solid green',
+        overflowY: 'scroll',
+        padding: '20px',
+      }}
+      justifyContent="flex-start"
+      flexDirection="column"
+    >
+      <div>HOME</div>
+      <div>HOME</div>
+      <div>HOME</div>
+      <div>HOME</div>
+      <div>HOME</div>
+      <div>HOME</div>
+      <div>HOME</div>
+      <div>HOME</div>
+      <div>HOME</div>
+      <div>HOME</div>
+      <div>HOME</div>
+      <div>HOME</div>
+      <div>HOME</div>
+      <div>HOME</div>
+      <div>HOME</div>
+      <div>HOME</div>
+      <div>HOME</div>
+      <div>HOME</div>
+      <div>HOME</div>
+      <div>HOME</div>
+      <div>HOME</div>
+      <div>HOME</div>
+      <div>HOME</div>
+      <div>HOME</div>
+      <div>HOME</div>
+      <div>HOME</div>
+      <div>HOME</div>
+      <div>HOME</div>
+      <div>HOME</div>
+      <div>HOME</div>
+      <div>HOME</div>
+      <div>HOME</div>
+      <div>HOME</div>
+      <div>HOME</div>
+      <div>HOME</div>
+      <div>HOME</div>
+      <div>HOME</div>
+      <div>HOME</div>
+      <div>HOME</div>
+      <div>HOME</div>
+      <div>HOME</div>
+      <div>HOME</div>
+      <div>HOME</div>
+      <div>HOME</div>
+      <div>HOME</div>
+      <div>HOME</div>
+      <div>HOME</div>
+      <div>HOME</div>
+      <div>HOME</div>
+      <div>HOME</div>
+    </LayoutWrap>
+  );
+};
+
+export default Home;
