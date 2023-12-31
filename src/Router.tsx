b01c1bb424b746cbcdb41000ebec6d583a447bb3
ParkYoDo <scarletdemon@naver.com>
2023-12-31 14:31:08 +0900

    rename : move layout directory

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,5 +1,5 @@
 import { Routes, Route } from 'react-router-dom';
-import Layout from '@pages/Layout/Layout';
+import Layout from '@components/Layout/Layout';
 import Home from '@pages/Home/Home';
 
 const Router = () => {
