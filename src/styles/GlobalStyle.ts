9f670a79615ac8be692a1cd80715abdeb88697a3
ParkYoDo <scarletdemon@naver.com>
2023-12-31 14:32:01 +0900

    design : add global-style

--- src/styles/GlobalStyle.ts
+++ src/styles/GlobalStyle.ts
@@ -1,28 +1,49 @@
-import styled, { createGlobalStyle } from 'styled-components';
+import styled, { createGlobalStyle, css } from 'styled-components';
+import { globalCssVariables } from './globalCssVariables';
 import { CSSProperties } from 'react';
 
 export const GlobalStyle = createGlobalStyle`
+    :root {
+        ${globalCssVariables}
+    }
+
     * {
         padding: 0;
         margin: 0;
         box-sizing: border-box;
-        
+      }
+
+    html, #root {
+      height: 100%;
     }
-    
+
     body {
+        height: 100%;
         font-size: 62.5%;
         font-family: 'Poor Story';
+        transition: color, background-color 0.25s ease-in-out;
+    }
+
+    body[data-theme='light'] {
+        color:#1a1a1a;
+        background-color: #f0f0f0;
+    }
+    body[data-theme='dark'] {
+        color: #f0f0f0;
+        background-color: #1a1a1a;
     }
 
     a {
         text-decoration: none;
-        color:white;
+        color: inherit;
     }
 `;
 
 export const LayoutWrap = styled.div<CSSProperties>`
   width: ${props => props?.width || '100%'};
   height: ${props => props?.height || '100%'};
+  padding: ${props => props?.padding || '0'};
+  margin: ${props => props?.margin || '0'};
   display: ${props => props?.display || 'flex'};
   flex-direction: ${props => props?.flexDirection || 'row'};
   justify-content: ${props => props?.justifyContent || 'center'};
