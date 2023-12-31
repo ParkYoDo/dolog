3254aae9d9c7c61ac61b7d5a68d877004f234871
ParkYoDo <scarletdemon@naver.com>
2023-12-31 14:28:22 +0900

    feat : navigationbar, darkmode

--- src/components/NavigationBar/NavigationBar.tsx
+++ src/components/NavigationBar/NavigationBar.tsx
@@ -1,14 +1,34 @@
 import { LayoutWrap } from '@styles/GlobalStyle';
-import { Link } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import * as S from '@components/NavigationBar/NavigationBarStyle';
 
 const NavigationBar = () => {
+  const [theme, setTheme] = useState(localStorage.getItem('theme'));
+
+  const onToggleTheme = () => {
+    if (theme === 'dark') {
+      return setTheme('light');
+    }
+    if (theme === 'light') {
+      return setTheme('dark');
+    }
+  };
+
+  useEffect(() => {
+    if (theme) {
+      localStorage.setItem('theme', theme);
+      document.body.dataset.theme = theme;
+    }
+  }, [theme]);
+
   return (
-    <LayoutWrap
-      height="70px"
-      style={{ background: 'black', color: 'white', letterSpacing: '10px' }}
-    >
-      <Link to="/">PARKYODO.Log</Link>
-    </LayoutWrap>
+    <S.NavigationBar theme={theme!}>
+      <S.NavLink to="/">Blog</S.NavLink>
+      <S.Menus>
+        <div>검색 아이콘</div>
+        <S.Theme onClick={onToggleTheme} darkMode={theme === 'dark'}></S.Theme>
+      </S.Menus>
+    </S.NavigationBar>
   );
 };
 
