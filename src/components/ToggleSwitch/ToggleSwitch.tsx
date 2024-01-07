e118dd04f5eb6ff92cae21a7128c2ace38261007
ParkYoDo <scarletdemon@naver.com>
2024-01-08 03:01:49 +0900

    fix : checked property without onChange

--- src/components/ToggleSwitch/ToggleSwitch.tsx
+++ src/components/ToggleSwitch/ToggleSwitch.tsx
@@ -2,14 +2,14 @@ import * as S from '@components/ToggleSwitch/ToggleSwitchStyle';
 
 interface Iprops {
   onClick: () => void;
-  checked?: boolean;
+  checked: boolean;
 }
 
 const ToggleSwitch = (props: Iprops) => {
   return (
     <>
-      <S.Input type="checkbox" id="switch" checked={props?.checked} />
-      <S.Label htmlFor="switch" onClick={props.onClick} />
+      <S.Input type="checkbox" id="switch" onChange={props?.onClick} checked={props.checked} />
+      <S.Label htmlFor="switch" />
     </>
   );
 };
