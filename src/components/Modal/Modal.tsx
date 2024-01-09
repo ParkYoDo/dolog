7ec67fcf00267dadc61233e137f5e2e249b5e2a5
ParkYoDo <scarletdemon@naver.com>
2024-01-10 05:04:42 +0900

    feat : react potal

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,30 +1,33 @@
-import { CSSProperties } from 'react';
+import { ReactNode } from 'react';
 import Overlay from '@components/Overlay/Overlay';
 import { LayoutWrap } from '@styles/GlobalStyle';
 import * as S from '@components/Modal/ModalStyle';
+import ModalPortal from '@components/Modal/ModalPortal';
 
 interface IProps {
   width?: string;
   height?: string;
   title: string;
   content: React.ReactNode;
-  buttons: any;
+  buttons?: ReactNode[];
 }
 
 const Modal = (props: IProps) => {
   return (
-    <Overlay>
-      <LayoutWrap
-        width={props.width || 'fit-content'}
-        height={props.height || 'fit-content'}
-        flexDirection="column"
-        style={{ border: '1px solid red' }}
-      >
-        <S.Header>{props.title}</S.Header>
-        <S.Body>{props.content}</S.Body>
-        {!!props?.buttons && <S.Footer></S.Footer>}
-      </LayoutWrap>
-    </Overlay>
+    <ModalPortal>
+      <Overlay>
+        <LayoutWrap
+          width={props.width || 'fit-content'}
+          height={props.height || 'fit-content'}
+          flexDirection="column"
+          style={{ border: '1px solid red' }}
+        >
+          <S.Header>{props.title}</S.Header>
+          <S.Body>{props.content}</S.Body>
+          {!!props?.buttons && <S.Footer></S.Footer>}
+        </LayoutWrap>
+      </Overlay>
+    </ModalPortal>
   );
 };
 
