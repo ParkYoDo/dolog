6f23a6cd20bf7d9ca1ebb42f7bdc8d92a0cd5f0f
parkyodo <scarletdemon@naver.com>
2024-01-10 19:19:54 +0900

    style : darkmode color

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -6,7 +6,9 @@ export const lightMode = `
 `;
 export const darkMode = `
   --text-color: #f0f0f0;
-  --background-color: #1a1a1a;
+  --background-color: #1c1e22;
   --border-color: 1px solid #f0f0f0;
   --navigation-color: #2e3137;
+  --modal-header-color: #3f4249;
+  --modal-body-color: #2e3137;
 `;
