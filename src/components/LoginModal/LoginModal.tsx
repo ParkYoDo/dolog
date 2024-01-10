7f379c9de2ac5f0a1c133e3dcfa961126b396f85
parkyodo <scarletdemon@naver.com>
2024-01-10 19:17:46 +0900

    feat : add login modal

---
+++ src/components/LoginModal/LoginModal.tsx
@@ -0,0 +1,8 @@
+import Modal from '@components/Modal/Modal';
+
+const LoginModal = () => {
+  const content = <></>;
+  return <Modal title="Login" content={content} />;
+};
+
+export default LoginModal;
--- src/components/NavigationBar/NavigationBar.tsx
+++ src/components/NavigationBar/NavigationBar.tsx
@@ -1,16 +1,28 @@
 import * as S from '@components/NavigationBar/NavigationBarStyle';
 import ToggleSwitch from '@components/ToggleSwitch/ToggleSwitch';
 import useTheme from '@hooks/useTheme';
+import LoginModal from '@components/LoginModal/LoginModal';
+import useModal from '@hooks/useModal';
+import { LayoutWrap } from '@styles/GlobalStyle';
 
 const NavigationBar = () => {
   const { theme, onToggleTheme } = useTheme();
+  const { onOpenModal } = useModal();
 
   return (
     <S.NavigationBar>
       <S.NavLink to="/">Blog</S.NavLink>
       <S.NavMenus>
         <ToggleSwitch onClick={onToggleTheme} checked={theme === 'dark'} />
-        <div>로그인</div>
+        <LayoutWrap
+          width="fit-content"
+          style={{ cursor: 'pointer' }}
+          onClick={() => {
+            onOpenModal(<LoginModal />);
+          }}
+        >
+          Login
+        </LayoutWrap>
       </S.NavMenus>
     </S.NavigationBar>
   );
