6f4aff0439db5a898d216a5a8dab50481b23411a
ParkYoDo <scarletdemon@naver.com>
2024-01-18 02:51:26 +0900

    rename: useAuth -> authApi

--- src/apis/apis.ts
+++ src/apis/apis.ts
@@ -1,18 +1,28 @@
-import apis from '@apis/api';
 import PATH from '@apis/apiPath';
-import { IAuth } from '@type/interface';
+import axiosInstance from '@apis/axiosInstance';
+import { IAuthLogin, IAuthSignUp } from '@type/authInterface';
+import { AxiosRequestConfig } from 'axios';
 
-export const authApi = {
-  signUp: async (data: IAuth) => {
-    const res = await apis.post(PATH.AUTHENTICATION.SIGNUP, data);
+const api = {
+  get: (url: string) => axiosInstance.get(url),
+  post: (url: string, data?: any, headers?: any) => axiosInstance.post(url, data, headers),
+  put: (url: string, data?: any) => axiosInstance.put(url, data),
+  delete: (url: string, data?: any) => axiosInstance.delete(url, { data: data }),
+  patch: (url: string, data?: any, config?: AxiosRequestConfig) =>
+    axiosInstance.patch(url, data, config),
+};
+
+export const auth = {
+  signUp: async (data: IAuthSignUp) => {
+    const res = await api.post(PATH.AUTHENTICATION.SIGNUP, data);
     return res.data;
   },
-  signIn: async (data: any) => {
-    const res = await apis.post(PATH.AUTHENTICATION.SIGNIN, data);
+  signIn: async (data: IAuthLogin) => {
+    const res = await api.post(PATH.AUTHENTICATION.SIGNIN, data);
     return res.data;
   },
   silentRefresh: async () => {
-    const res = await apis.post(PATH.AUTHENTICATION.SILENT_REFRESH);
+    const res = await api.post(PATH.AUTHENTICATION.SILENT_REFRESH);
     return res.data;
   },
 };
