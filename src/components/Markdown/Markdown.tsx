84d0b78e28d2cb8b129cba9a29a6d8edc167a589
parkyodo <scarletdemon@naver.com>
2024-01-23 17:48:20 +0900

    design : change react-markdown html tag style

--- src/components/Markdown/Markdown.tsx
+++ src/components/Markdown/Markdown.tsx
@@ -1,4 +1,5 @@
 import { LayoutWrap } from '@styles/GlobalStyle';
+import { useRef, useState } from 'react';
 import ReactMarkdown from 'react-markdown';
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
 import { vscDarkPlus } from 'react-syntax-highlighter/dist/esm/styles/prism';
@@ -11,28 +12,37 @@ const Markdown = ({ title, content }: { title: string; content: string }) => {
       <TitleViewer>{title}</TitleViewer>
       <ContentViewer>
         <ReactMarkdown
+          children={
+            content
+              .replace(/\n/gi, '\n\n')
+              .replace(/\*\*/gi, '@$_%!^')
+              .replace(/@\$_%!\^/gi, '**')
+              .replace(/<\/?u>/gi, '*') // /u or u => *
+          }
           remarkPlugins={[remarkGfm]}
           components={{
-            // 코드
-            code({ className, children }) {
-              const match = /language-(\w+)/.exec(className || '');
-              return match ? (
-                <SyntaxHighlighter style={vscDarkPlus} language={match[1]} PreTag="div">
-                  {String(children)}
-                </SyntaxHighlighter>
-              ) : (
-                <SyntaxHighlighter
-                  style={vscDarkPlus}
-                  background="green"
-                  language="textile"
-                  PreTag="div"
-                >
-                  {String(children).replace(/\n$/, '')}
-                </SyntaxHighlighter>
-              );
+            h1: props => {
+              return <h1 style={{ fontWeight: 'bold', fontSize: '2.5rem' }} {...props} />;
             },
-            // 인용문 (>)
-            blockquote({ children, ...props }) {
+            h2: props => {
+              return <h2 style={{ fontWeight: 'bold', fontSize: '2.0rem' }} {...props} />;
+            },
+            h3: props => {
+              return <h3 style={{ fontWeight: 'bold', fontSize: '1.5rem' }} {...props} />;
+            },
+            h4: props => {
+              return <h4 style={{ fontWeight: 'bold', fontSize: '1.125rem' }} {...props} />;
+            },
+            strong: props => {
+              return <strong {...props} />;
+            },
+            em: props => {
+              return <em {...props} />;
+            },
+            del: props => {
+              return <del {...props} />;
+            },
+            blockquote: props => {
               return (
                 <blockquote
                   style={{
@@ -41,35 +51,41 @@ const Markdown = ({ title, content }: { title: string; content: string }) => {
                     borderRadius: '10px',
                   }}
                   {...props}
-                >
-                  {children}
-                </blockquote>
-              );
-            },
-            img({ ...props }) {
-              return (
-                <img
-                  style={{ maxWidth: '40vw' }}
-                  src={props.src?.replaceAll('%3A', ':')}
-                  alt="MarkdownRenderer__Image"
                 />
               );
             },
-            em({ children, ...props }) {
+            a: props => {
+              return <a style={{ color: 'blue' }} {...props} />;
+            },
+            img: props => {
               return (
-                <span style={{ fontStyle: 'italic' }} {...props}>
-                  {children}
-                </span>
+                <img
+                  style={{ maxWidth: '100%' }}
+                  src={props.alt ? '' : props.src?.replaceAll('%3A', ':')}
+                  alt="Image"
+                />
+              );
+            },
+            pre: props => <pre style={{ border: '3px solid red' }} {...props} />,
+            code: props => {
+              const match = /language-(\w+)/.exec(props.className || '');
+              return match ? (
+                <SyntaxHighlighter style={vscDarkPlus} language={match[1]} PreTag="div">
+                  {String(props.children)}
+                </SyntaxHighlighter>
+              ) : (
+                <SyntaxHighlighter
+                  style={vscDarkPlus}
+                  background="green"
+                  language="textile"
+                  PreTag="div"
+                >
+                  {String(props.children).replace(/\n$/, '')}
+                </SyntaxHighlighter>
               );
             },
           }}
-        >
-          {content
-            .replace(/\n/gi, '\n\n')
-            .replace(/\*\*/gi, '@$_%!^')
-            .replace(/@\$_%!\^/gi, '**')
-            .replace(/<\/?u>/gi, '*')}
-        </ReactMarkdown>
+        />
       </ContentViewer>
     </MarkDownstyle>
   );
@@ -93,6 +109,26 @@ const ContentViewer = styled(LayoutWrap)`
   padding: 24px;
   line-height: 1.7rem;
   overflow-y: auto;
+
+  p:has(img) {
+    width: 100%;
+    border: 1px solid red;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  pre {
+    width: 100%;
+  }
+
+  pre > div {
+    border-radius: 12px;
+    width: 100%;
+  }
+
+  code {
+    width: 100%;
+  }
 `;
 
 const MarkDownstyle = styled(LayoutWrap)`
